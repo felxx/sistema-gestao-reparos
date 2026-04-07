d1aedb84a554f82efe36d289ec12a0120eca5323
felxx <igorfelipemonteirocontato@gmail.com>
2026-04-07 15:27:58 -0300

    feat: cria a página sobre do projeto

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class PaginaInicialView(TemplateView):
+    template_name = "cadastros/index.html"
