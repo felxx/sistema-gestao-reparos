32324ae85e8d844a0756bfede7e7f958a9574b81
felxx <igorfelipemonteirocontato@gmail.com>
2026-04-21 17:51:24 -0300

    feat: adiciona views e templates para cadastro de cliente e equipamento

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PaginaInicialView
+from .views import PaginaInicialView, ClienteCreate, EquipamentoCreate
 
 urlpatterns = [
     path('', PaginaInicialView.as_view(), name="index"),
+    path('cadastrar/cliente/', ClienteCreate.as_view(), name="cadastrar-cliente"),
+    path('cadastrar/equipamento/', EquipamentoCreate.as_view(), name="cadastrar-equipamento"),
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,19 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Cliente, Equipamento
 
 class PaginaInicialView(TemplateView):
     template_name = "cadastros/index.html"
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = ['nome', 'cpf_cnpj', 'telefone', 'endereco']
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('index')
+
+class EquipamentoCreate(CreateView):
+    model = Equipamento
+    fields = ['cliente', 'tipo', 'marca', 'modelo', 'numero_serie', 'especificacoes']
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('index')
